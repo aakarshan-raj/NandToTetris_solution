04c335c2b00e1654f4dc211a4f55d2754377319b
aakarshan-raj <aakarshan7raj@gmail.com>
2023-11-23 17:39:44 +0530

    draw on screen

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,24 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAIN)   //read keyboard register
+@KBD
+D=M
+
+@WHITE   //jump if 0
+D;JEQ
+
+@SCREEN  //continue execution 
+D=A
+@R0
+M=D
+(LOOP_BLACK)
+@24576
+D=A
+@R0
+D=D-M
+@MAIN    //get out after screen is black  
+D;JEQ
+@R0
+D=M 
