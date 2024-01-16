a212d17a060857280cf84f01da7a315ec8f9a552
aakarshan-raj <aakarshan7raj@gmail.com>
2024-01-16 20:27:11 +0530

    correct Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,18 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
-
 (MAIN)   //read keyboard register
 @KBD
 D=M
@@ -33,3 +18,43 @@ D=D-M
 D;JEQ
 @R0
 D=M 
+A=D
+M=-1   //CHANGE TO BLACK
+@R0
+A=M+1
+D=A
+@R0 
+M=D
+@LOOP_BLACK
+0;JMP
+
+@MAIN
+0;JMP
+
+(WHITE)
+@SCREEN
+D=A
+@R0
+M=D
+(LOOP_WHITE)
+@24576
+D=A
+@R0
+D=D-M
+@MAIN   //get out after screen is white
+D;JEQ
+@R0
+D=M 
+A=D
+M=0   //CHANGE TO WHITE
+@R0
+A=M+1
+D=A
+@R0 
+M=D
+@LOOP_WHITE
+0;JMP
+
+@MAIN  //loop back
+0;JMP
+
